c7a3774d55df1adf63faa87bbfc6198c53fbcfae
frtnite <penguingamer1212@gmail.com>
2019-03-13 18:44:07 +0800

    first push

--- L7/app.js
+++ L7/app.js
@@ -3,7 +3,7 @@ var life = 3;
 var score = 0;
 
 function preload(){
-	game.load.image('sky', 'assets/sky.png');
+	game.load.image('sky','assets/sky.png');
 	game.load.image('ground','assets/platform.png');
 	game.load.image('star','asset/star.png');
 	game.load.spritesheet('dude','assets/dude.png', 32, 48);
